b051a2841606360aa9ec479e73df41fdbdd81a95
lazynina <lazynina84@gmail.com>
2021-05-02 12:37:35 -0700

    fix message for dialog when user tries to diamond without a profile

--- src/lib/shared-dialogs.ts
+++ src/lib/shared-dialogs.ts
@@ -1,6 +1,7 @@
 import RouteNamesService from "../app/route-names.service";
 import { SwalHelper } from "./helpers/swal-helper";
 import { GlobalVarsService } from "../app/global-vars.service";
+import { Router } from "@angular/router";
 
 const RouteNames = RouteNamesService;
 export class SharedDialogs {
@@ -26,10 +27,32 @@ export class SharedDialogs {
     });
   }
 
-  static showCreateProfileToPostDialog(router) {
+  static showCreateProfileToPostDialog(router: Router) {
     SwalHelper.fire({
       icon: "info",
       title: `Complete your profile to post`,
+      html: `You can be whoever you want to be.`,
+      showCancelButton: true,
+      showConfirmButton: true,
+      focusConfirm: true,
+      customClass: {
+        confirmButton: "btn btn-light",
+        cancelButton: "btn btn-light no",
+      },
+      confirmButtonText: "Complete Your Profile",
+      cancelButtonText: "Nevermind",
+      reverseButtons: true,
+    }).then((res: any) => {
+      if (res.isConfirmed) {
+        router.navigate(["/" + RouteNames.UPDATE_PROFILE], { queryParamsHandling: "merge" });
+      }
+    });
+  }
+
+  static showCreateProfileToPerformActionDialog(router: Router, action: string) {
+    SwalHelper.fire({
+      icon: "info",
+      title: `Complete your profile to ${action}`,
       html: `You can be whoever you want to be.`,
       showCancelButton: true,
       showConfirmButton: true,
