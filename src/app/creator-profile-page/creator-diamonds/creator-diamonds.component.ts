7957f754f40aed3bd5297df4a35c251230397a77
Lazy Nina <81658138+lazynina@users.noreply.github.com>
2021-05-03 13:45:10 -0700

    Ln/add coin price to diamond tab (#19)
    * initial checkin for adding coin price to diamonds tba
    
    * add coin price under username in diamond tab, fix username truncation for mobile v desktop in diamond tab and wallet

--- src/app/creator-profile-page/creator-diamonds/creator-diamonds.component.ts
+++ src/app/creator-profile-page/creator-diamonds/creator-diamonds.component.ts
@@ -14,27 +14,33 @@ export class CreatorDiamondsComponent implements OnInit {
   diamondSummaryList = [];
   totalDiamonds = 0;
 
-  constructor(
-    private _globalVars: GlobalVarsService,
-    private backendApi: BackendApiService,
-  ) { this.globalVars = _globalVars; }
+  constructor(private _globalVars: GlobalVarsService, private backendApi: BackendApiService) {
+    this.globalVars = _globalVars;
+  }
 
   ngOnInit(): void {
-    this.isLoading = true
-    this.backendApi.GetDiamondsForPublicKey(
-      this.globalVars.localNode,
-      this.profile.PublicKeyBase58Check,
-    ).subscribe(
-      (res) => {
-        this.diamondSummaryList = res.DiamondSenderSummaryResponses
-        this.totalDiamonds = res.TotalDiamonds;
-      }, (err) => {
-        this.globalVars._alertError(this.backendApi.parseProfileError(err))        
-      },
-    ).add(() => { this.isLoading = false; })
+    this.isLoading = true;
+    this.backendApi
+      .GetDiamondsForPublicKey(this.globalVars.localNode, this.profile.PublicKeyBase58Check)
+      .subscribe(
+        (res) => {
+          this.diamondSummaryList = res.DiamondSenderSummaryResponses;
+          this.totalDiamonds = res.TotalDiamonds;
+        },
+        (err) => {
+          this.globalVars._alertError(this.backendApi.parseProfileError(err));
+        }
+      )
+      .add(() => {
+        this.isLoading = false;
+      });
   }
 
   counter(num: number) {
-    return Array(num)
+    return Array(num);
+  }
+
+  usernameTruncationLength(): number {
+    return this.globalVars.isMobile() ? 10 : 12;
   }
 }
