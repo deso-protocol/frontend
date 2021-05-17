34905e9ace68e5c1377e1e4ec5847008817442e1
lazynina <lazynina84@gmail.com>
2021-05-16 21:27:37 -0700

    adjust select size based on selected option

--- src/app/right-bar-creators/right-bar-creators.component.ts
+++ src/app/right-bar-creators/right-bar-creators.component.ts
@@ -1,29 +1,45 @@
 import { Component, OnInit } from "@angular/core";
 import { GlobalVarsService } from "../global-vars.service";
+import { BackendApiService } from "../backend-api.service";
+
+class RightBarTabOption {
+  name: string;
+  width: number;
+}
 
 @Component({
   selector: "right-bar-creators",
   templateUrl: "./right-bar-creators.component.html",
   styleUrls: ["./right-bar-creators.component.sass"],
 })
-export class RightBarCreatorsComponent {
-  constructor(public globalVars: GlobalVarsService) {}
+export class RightBarCreatorsComponent implements OnInit {
+  constructor(public globalVars: GlobalVarsService, private backendApi: BackendApiService) {}
 
-  chartOptions = [
-    { name: "Gainers", width: 100 },
-    { name: "Diamonded Creators", width: 200 },
-    { name: "Community Projects", width: 200 },
-  ];
+  activeTab: string;
 
-  chartWidthMap = {
-    Gainers: 100,
-    "Diamonded Creators": 200,
-    "Community Projects": 200,
+  static RightBarTabKey = "RightBarTab";
+
+  static GAINERS: RightBarTabOption = { name: "Gainers", width: 90 };
+  static DIAMONDS: RightBarTabOption = { name: "Diamonded Creators", width: 190 };
+  static COMMUNITY: RightBarTabOption = { name: "Community Projects", width: 190 };
+
+  chartMap = {
+    [RightBarCreatorsComponent.GAINERS.name]: RightBarCreatorsComponent.GAINERS,
+    [RightBarCreatorsComponent.DIAMONDS.name]: RightBarCreatorsComponent.DIAMONDS,
+    [RightBarCreatorsComponent.COMMUNITY.name]: RightBarCreatorsComponent.COMMUNITY,
   };
-  setSelectWidth(selectedTab: string) {
-    // console.log(width);
-    const width = this.chartWidthMap[selectedTab];
+
+  ngOnInit() {
+    const defaultTab = this.backendApi.GetStorage(RightBarCreatorsComponent.RightBarTabKey);
+    this.activeTab = defaultTab in this.chartMap ? defaultTab : RightBarCreatorsComponent.GAINERS.name;
+    this.selectTab();
+  }
+
+  selectTab() {
+    console.log(JSON.stringify(this.activeTab));
+    const rightTabOption = this.chartMap[this.activeTab];
     const selectElement = document.getElementById("right-bar-chart-select");
-    selectElement.style.width = width + "px";
+    selectElement.style.width = rightTabOption.width + "px";
+    this.backendApi.SetStorage(RightBarCreatorsComponent.RightBarTabKey, this.activeTab);
   }
 }
