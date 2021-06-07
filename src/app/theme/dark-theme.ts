ffdf7eb61d4001fb302a2df23370d4f6ba22803b
carsenk <carsenk@gmail.com>
2021-06-07 00:12:55 -0600

    Theme Updates bring more inline with current light mode and updates to dark

--- src/app/theme/dark-theme.ts
+++ src/app/theme/dark-theme.ts
@@ -3,11 +3,16 @@ import { Theme } from './symbols';
 export const darkTheme: Theme = {
   name: 'dark',
   properties: {
-    '--background': '#000',
-    '--text': 'white',
-    '--secondary': '#222',
+    '--background': '#121212',
+    '--text': '#EFF3F8',
+    '--grey': '#999',
+    '--secondary': '#202020',
     '--secalt': '#111',
     '--textalt': '#eee',
+    '--norm': '#eee',
+    '--formbg': '#111',
+    '--link': '#4DA3FF',
+    '--hover': '#72b6ff',
     '--filter': 'invert(98%) sepia(1%) saturate(264%) hue-rotate(181deg) brightness(116%) contrast(100%)',
     '--unread': '#1a293f',
   }
--- src/app/theme/light-theme.ts
+++ src/app/theme/light-theme.ts
@@ -4,10 +4,15 @@ export const lightTheme: Theme = {
   name: 'light',
   properties: {
     '--background': '#fff',
-    '--text': '#333',
+    '--text': '#333333',
+    '--grey': '#555555',
     '--secondary': '#f8f8f8',
     '--secalt': '#eee',
     '--textalt': '#777',
+    '--norm': '#222',
+    '--formbg': '#fafafa',
+    '--link': '#007BFF',
+    '--hover': '#0056b3',
     '--filter': 'invert(0%) sepia(5%) saturate(7493%) hue-rotate(217deg) brightness(105%) contrast(101%)',
     '--unread': '#e7effe',
   }
