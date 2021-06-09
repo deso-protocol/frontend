756d876e2d023a9f06e709342ddaae5028445aa8
Carsen Klock <carsenk@gmail.com>
2021-06-09 16:02:56 -0600

    Fixes to comment-modal for reply modal styling from themes

--- src/app/comment-modal/comment-modal.component.ts
+++ src/app/comment-modal/comment-modal.component.ts
@@ -11,6 +11,14 @@ export class CommentModalComponent {
   @Input() parentPost;
   @Input() afterCommentCreatedCallback: any = null;
   @Input() isQuote = false;
+  
+  ngOnInit() {
+    if (localStorage.getItem("theme") == "light") {
+      this.bsModalRef.setClass('lightmodal');
+    } else {
+      this.bsModalRef.setClass('darkmodal');
+    }
+  }
 
   constructor(public bsModalRef: BsModalRef) {}
 }
