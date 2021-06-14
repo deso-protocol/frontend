dc990a048347e4ab4148f118a04c71d19edcbc75
redpartyhat <notredpartyhat@gmail.com>
2021-06-13 22:34:19 -0700

    Add transfer amounts to notifications.

--- src/app/notifications-page/notifications-list/notifications-list.component.ts
+++ src/app/notifications-page/notifications-list/notifications-list.component.ts
@@ -181,8 +181,15 @@ export class NotificationsListComponent implements OnInit {
     };
 
     if (txnMeta.TxnType === "BASIC_TRANSFER") {
+      let txnAmountNanos = 0;
+      for (let ii = 0; ii < notification.TxnOutputResponses.length; ii++) {
+        if (notification.TxnOutputResponses[ii].PublicKeyBase58Check === userPublicKeyBase58Check) {
+          txnAmountNanos += notification.TxnOutputResponses[ii].AmountNanos;
+        }
+      }
       result.icon = "fas fa-money-bill-wave-alt fc-green";
-      result.action = `${actorName} sent you BitClout!</b>`;
+      result.action = `${actorName} sent you ${this.globalVars.nanosToBitClout(txnAmountNanos)} ` +
+        `BitClout!</b> (~${this.globalVars.nanosToUSD(txnAmountNanos, 2)})`;
       return result;
     } else if (txnMeta.TxnType === "CREATOR_COIN") {
       // If we don't have the corresponding metadata then return null.
