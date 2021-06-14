76858ff2bde9a4b5863502a571b5a74d22d518ea
Lazy Nina <81658138+lazynina@users.noreply.github.com>
2021-06-13 22:58:21 -0700

    add typing for the properties of a theme (#157)

--- src/app/theme/symbols.ts
+++ src/app/theme/symbols.ts
@@ -6,7 +6,27 @@ export const ACTIVE_THEME = new InjectionToken("ACTIVE_THEME");
 export interface Theme {
   key: string;
   name: string;
-  properties: any;
+  properties: {
+    "--background": string;
+    "--text": string;
+    "--grey": string;
+    "--secondary": string;
+    "--secalt": string;
+    "--textalt": string;
+    "--norm": string;
+    "--formbg": string;
+    "--link": string;
+    "--hover": string;
+    "--border": string;
+    "--mborder": string;
+    "--filter": string;
+    "--unread": string;
+    "--topbar": string;
+    "--cblue": string;
+    "--cred": string;
+    "--cgreen": string;
+    "--loading": string;
+  };
 }
 
 export interface ThemeOptions {
