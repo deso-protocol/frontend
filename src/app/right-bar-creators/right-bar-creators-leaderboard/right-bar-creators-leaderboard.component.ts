db97a22ea18a1a2c3827c61bae51aae6f9f3d861
Lazy Nina <81658138+lazynina@users.noreply.github.com>
2021-06-14 13:26:33 -0700

    force refresh of the leaderboard when loading the trends page (#164)

--- src/app/right-bar-creators/right-bar-creators-leaderboard/right-bar-creators-leaderboard.component.ts
+++ src/app/right-bar-creators/right-bar-creators-leaderboard/right-bar-creators-leaderboard.component.ts
@@ -18,71 +18,9 @@ export class RightBarCreatorsLeaderboardComponent implements OnInit {
 
   RightBarCreatorsComponent = RightBarCreatorsComponent;
 
-  constructor(
-    public globalVars: GlobalVarsService,
-    private route: ActivatedRoute,
-    private _router: Router,
-    private backendApi: BackendApiService,
-    private httpClient: HttpClient
-  ) {}
+  constructor(public globalVars: GlobalVarsService, private route: ActivatedRoute, private _router: Router) {}
 
   ngOnInit() {
-    if (this.globalVars.rightBarLeaderboard.length > 0) {
-      return;
-    }
-
-    const pulseService = new PulseService(this.httpClient, this.backendApi, this.globalVars);
-
-    if (this.globalVars.topGainerLeaderboard.length === 0) {
-      pulseService.getBitCloutLockedLeaderboard().subscribe((res) => (this.globalVars.topGainerLeaderboard = res));
-    }
-    if (this.globalVars.topDiamondedLeaderboard.length === 0) {
-      pulseService.getDiamondsReceivedLeaderboard().subscribe((res) => (this.globalVars.topDiamondedLeaderboard = res));
-    }
-
-    const bithuntService = new BithuntService(this.httpClient, this.backendApi, this.globalVars);
-    if (this.globalVars.topCommunityProjectsLeaderboard.length === 0) {
-      bithuntService.getCommunityProjectsLeaderboard().subscribe((res) => {
-        this.globalVars.allCommunityProjectsLeaderboard = res;
-        this.globalVars.topCommunityProjectsLeaderboard = this.globalVars.allCommunityProjectsLeaderboard.slice(0, 10);
-      });
-    }
-
-    if (this.globalVars.topCreatorsAllTimeLeaderboard.length === 0) {
-      let readerPubKey = "";
-      if (this.globalVars.loggedInUser) {
-        readerPubKey = this.globalVars.loggedInUser.PublicKeyBase58Check;
-      }
-      this.backendApi
-        .GetProfiles(
-          this.globalVars.localNode,
-          null /*PublicKeyBase58Check*/,
-          null /*Username*/,
-          null /*UsernamePrefix*/,
-          null /*Description*/,
-          BackendApiService.GET_PROFILES_ORDER_BY_INFLUENCER_COIN_PRICE /*Order by*/,
-          10 /*NumEntriesToReturn*/,
-          readerPubKey /*ReaderPublicKeyBase58Check*/,
-          "leaderboard" /*ModerationType*/,
-          false /*FetchUsersThatHODL*/,
-          false /*AddGlobalFeedBool*/
-        )
-        .subscribe(
-          (response) => {
-            this.globalVars.topCreatorsAllTimeLeaderboard = response.ProfilesFound.slice(
-              0,
-              RightBarCreatorsLeaderboardComponent.MAX_PROFILE_ENTRIES
-            ).map((profile) => {
-              return {
-                Profile: profile,
-              };
-            });
-          },
-          (err) => {
-            console.error(err);
-            this.globalVars._alertError("Error loading profiles: " + this.backendApi.stringifyError(err));
-          }
-        );
-    }
+    this.globalVars.updateLeaderboard();
   }
 }
--- src/app/trends-page/trends/trends.component.ts
+++ src/app/trends-page/trends/trends.component.ts
@@ -70,12 +70,7 @@ export class TrendsComponent implements OnInit {
   }
 
   ngOnInit() {
-    if (this.globalVars.allCommunityProjectsLeaderboard.length === 0) {
-      this.bithuntService.getCommunityProjectsLeaderboard().subscribe((res) => {
-        this.globalVars.allCommunityProjectsLeaderboard = res;
-        this.globalVars.topCommunityProjectsLeaderboard = this.globalVars.allCommunityProjectsLeaderboard.slice(0, 10);
-      });
-    }
+    this.globalVars.updateLeaderboard(true);
   }
 
   datasource: IDatasource<IAdapter<any>> = this.getDatasource();
@@ -131,7 +126,6 @@ export class TrendsComponent implements OnInit {
         .toPromise()
         .then(
           (res) => {
-            // this.pagedKeys[page + 1] = ;
             if (res.length < TrendsComponent.PAGE_SIZE) {
               this.lastPageByTab[this.activeTab] = page;
               this.lastPage = page;
