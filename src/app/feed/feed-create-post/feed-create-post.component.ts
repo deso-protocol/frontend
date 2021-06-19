086896fc96ab494b1fce24badcb8909da9ffc7a7
carsenk <carsenk@gmail.com>
2021-06-19 17:28:51 -0600

    New Emoji Picker for Posts

--- src/app/feed/feed-create-post/feed-create-post.component.ts
+++ src/app/feed/feed-create-post/feed-create-post.component.ts
@@ -6,6 +6,7 @@ import { SharedDialogs } from "../../../lib/shared-dialogs";
 import { CdkTextareaAutosize } from "@angular/cdk/text-field";
 import { EmbedUrlParserService } from "../../../lib/services/embed-url-parser-service/embed-url-parser-service";
 import { environment } from "../../../environments/environment";
+import { EmojiButton } from "@joeattardi/emoji-button";
 
 @Component({
   selector: "feed-create-post",
@@ -69,6 +70,25 @@ export class FeedCreatePostComponent implements OnInit {
   ngOnInit() {
     this.isComment = !this.isQuote && !!this.parentPost;
     this._setRandomMovieQuote();
+  }
+
+  emojiClick(event: any): void {
+    const trigger = event.currentTarget;
+    const picker = new EmojiButton({
+      theme: window.matchMedia("(prefers-color-scheme: dark)").matches ? "light" : "dark",
+      position: "auto",
+      autoHide: false,
+      emojiSize: "1.3em",
+      emojisPerRow: 6,
+      rows: 3,
+      initialCategory: "smileys",
+      showPreview: false,
+    });
+
+    picker.togglePicker(trigger);
+    picker.on("emoji", (selection) => {
+      this.postInput += selection.emoji;
+    });
   }
 
   onPaste(event: any): void {
