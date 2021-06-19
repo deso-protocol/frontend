f340828e590cad17c576b2040ba27aff74300d17
carsenk <carsenk@gmail.com>
2021-06-19 12:08:01 -0600

    Drag and Drop Image Uploads

---
+++ src/app/directives/upload.directive.ts
@@ -0,0 +1,35 @@
+import { Directive, Output, EventEmitter, HostBinding, HostListener, HostBindingDecorator } from "@angular/core";
+
+@Directive({
+  selector: "[dropUpload]",
+})
+export class UploadDirective {
+  @Output() onFileDropped = new EventEmitter<any>();
+  @HostBinding("style.opacity") public opacity = "1";
+
+  //Dragover listener, when something is dragged over our host element
+  @HostListener("dragover", ["$event"]) onDragOver(evt) {
+    evt.preventDefault();
+    evt.stopPropagation();
+    this.opacity = "0.5";
+  }
+
+  //Dragleave listener, when something is dragged away from our host element
+  @HostListener("dragleave", ["$event"]) public onDragLeave(evt) {
+    evt.preventDefault();
+    evt.stopPropagation();
+    this.opacity = "1";
+  }
+
+  @HostListener("drop", ["$event"]) public ondrop(evt) {
+    evt.preventDefault();
+    evt.stopPropagation();
+    this.opacity = "1";
+    let files = evt.dataTransfer.files;
+    if (files.length > 0) {
+      this.onFileDropped.emit(files);
+    }
+  }
+
+  constructor() {}
+}
