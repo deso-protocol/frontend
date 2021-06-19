0dcf59c32bb03af3efdb2162e38606ef27c41c97
Lazy Nina <81658138+lazynina@users.noreply.github.com>
2021-06-18 17:13:56 -0700

    Blacklisted profile fixes (#190)
    * if user is blacklisted, we redirect to not found page in creator profile detalis component
    
    * simplify not found logic

--- src/app/creator-profile-page/creator-profile-details/creator-profile-details.component.ts
+++ src/app/creator-profile-page/creator-profile-details/creator-profile-details.component.ts
@@ -166,9 +166,9 @@ export class CreatorProfileDetailsComponent {
     this.loading = true;
     this.backendApi.GetSingleProfile(this.globalVars.localNode, "", this.userName).subscribe(
       (res) => {
-        if (!res) {
-          console.log("This profile was not found. It either does not exist or it was deleted.");
+        if (!res || res.IsBlacklisted) {
           this.loading = false;
+          this.router.navigateByUrl("/" + this.appData.RouteNames.NOT_FOUND, { skipLocationChange: true });
           return;
         }
         this.profile = res.Profile;
