2f85b840db9146de365968e7d21bd45bd5226345
bluepartyhat <81657864+bluepartyhat@users.noreply.github.com>
2021-07-02 18:31:38 -0700

    Remove detect changes.
    Co-authored-by: Lazy Nina <81658138+lazynina@users.noreply.github.com>

--- src/app/feed/feed-post/feed-post.component.ts
+++ src/app/feed/feed-post/feed-post.component.ts
@@ -261,7 +261,6 @@ export class FeedPostComponent implements OnInit {
   }
 
   pinPostToProfile() {
-    this.ref.detectChanges();
     this.backendApi
       .SubmitPost(
         this.globalVars.localNode,
