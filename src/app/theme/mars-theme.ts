7402dd59711aafa8d5943cf17124819c4f9f0a00
BrixBoston100 <82254764+teambrixmusic@users.noreply.github.com>
2021-07-09 01:02:34 -0400

    Add mars-theme.ts to theme folder

---
+++ src/app/theme/mars-theme.ts
@@ -0,0 +1,28 @@
+import { Theme } from "./symbols";
+
+export const marsTheme: Theme = {
+  key: "mars",
+  name: "Mars Theme",
+  properties: {
+    "--background": "#521f20",
+    "--text": "#f5d4cb",
+    "--grey": "#c6a7a7",
+    "--secondary": "#672529",
+    "--secalt": "#741c24",
+    "--textalt": "#c6a7a7",
+    "--norm": "#c6a7a7",
+    "--formbg": "#741c24",
+    "--link": "#f44336",
+    "--hover": "#dc3545",
+    "--border": "#672529",
+    "--mborder": "672529",
+    "--filter": "brightness(116%) contrast(100%) hue-rotate(116deg) invert(98%) sepia(1%) saturate(264%)",
+    "--unread": "#672529",
+    "--topbar": "#bf3a30",
+    "--cblue": "#238eff",
+    "--cred": "#f44336",
+    "--cgreen": "#ffc107",
+    "--button": "#f44336",
+    "--loading": "#f44336",
+  },
+};
