fe8576b3e81c27bf8a750d71ac3eb3f448c35d88
JustifydWarrior <info@justifydwarrior.com>
2021-07-18 15:18:51 -0700

    fix themes

--- src/app/settings/settings.component.ts
+++ src/app/settings/settings.component.ts
@@ -32,7 +32,7 @@ export class SettingsComponent implements OnInit {
 
     this.themeService.setTheme(selectedTheme);
     localStorage.setItem("theme", selectedTheme);
-    document.querySelector("[app-theme]").className = "main global__container " + selectedTheme;
+    document.querySelector("[app-theme]").classList.add(selectedTheme);
   }
 
   ngOnInit() {
