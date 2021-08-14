340b234aee7fbee9e5467107df1d000ae8d8e0f6
Martijn van Halen <martijn.vanhalen@gmail.com>
2021-08-14 16:24:12 +0200

    Share Post

--- src/app/feed/feed-post-dropdown/feed-post-dropdown.component.ts
+++ src/app/feed/feed-post-dropdown/feed-post-dropdown.component.ts
@@ -188,6 +188,20 @@ export class FeedPostDropdownComponent {
     this.globalVars._copyText(this._getPostUrl());
   }
 
+  sharePostUrl(event){
+    this.globalVars.logEvent("post : webapishare");
+    
+    // Prevent the post from navigating.
+    event.stopPropagation();
+
+    try {
+      navigator.share({url: this._getPostUrl() });
+      console.log("Data was shared successfully");
+    } catch (err) {
+      console.error("Share failed:", err.message);
+    }
+  }
+
   _getPostUrl() {
     const pathArray = ["/" + this.globalVars.RouteNames.POSTS, this.postContent.PostHashHex];
 
