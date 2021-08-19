3219afbe09913e7f43ab9e562fb91dc055466127
lazynina <lazynina84@gmail.com>
2021-08-18 17:44:39 -0700

    testnet version of tutorial

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: false,
   uploadImageHostname: "bitclout.com",
-  jumioEndpointHostname: "localhost:18001",
+  jumioEndpointHostname: "bitclout.com",
   dd: {
     apiKey: "DCEB26AC8BF47F1D7B4D87440EDCA6",
     jsPath: "https://bitclout.com/tags.js",
