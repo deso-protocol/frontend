f0d84d118d85f7345652e06391ba6584eb134fb5
lazynina <lazynina84@gmail.com>
2021-08-24 21:07:47 -0400

    use mainnet post hash in tutorial

--- src/app/tutorial/diamond-tutorial-page/diamond-tutorial/diamond-tutorial.component.ts
+++ src/app/tutorial/diamond-tutorial-page/diamond-tutorial/diamond-tutorial.component.ts
@@ -20,8 +20,8 @@ export class DiamondTutorialComponent implements OnInit {
 
   post: PostEntryResponse;
   // Use this posthash in testnet.
-  postHashHex = "3e42215a120a6e9d4848117f5829a2c4d9f692360fd14b78daea483a72d142dc";
-  // postHashHex = "75f16239b57de0531f9579f3817beb0a67515e4999947f293c112fb0260178e4";
+  // postHashHex = "3e42215a120a6e9d4848117f5829a2c4d9f692360fd14b78daea483a72d142dc";
+  postHashHex = "75f16239b57de0531f9579f3817beb0a67515e4999947f293c112fb0260178e4";
   loading: boolean = true;
 
   ngOnInit() {
