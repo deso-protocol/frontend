3b64b2f29043a3e068d7064748f28adefea03c1a
lazynina <lazynina84@gmail.com>
2021-09-14 18:44:32 +0300

    fix function on enter keydown in remove verification badge, fix setting a bid as selected when selecting a winner for 1-of-1 NFTs

--- src/app/nft-post-page/nft-post/nft-post.component.ts
+++ src/app/nft-post-page/nft-post/nft-post.component.ts
@@ -280,6 +280,7 @@ export class NftPostComponent {
   }
 
   selectBidEntry(bidEntry: NFTBidEntryResponse): void {
+    this.bids.forEach((bidEntry) => (bidEntry.selected = false));
     bidEntry.selected = true;
     this.sellNFTDisabled = false;
   }
