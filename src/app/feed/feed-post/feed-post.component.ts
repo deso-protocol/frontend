3222a87f39bf294e603c7ae605cd5dd5c4bcc931
BitClout.com/u/tijn <69529928+tijno@users.noreply.github.com>
2021-09-18 16:49:37 +0100

    Add min bid to single nfts if greater then 0

--- src/app/feed/feed-post/feed-post.component.ts
+++ src/app/feed/feed-post/feed-post.component.ts
@@ -96,6 +96,7 @@ export class FeedPostComponent implements OnInit {
   @Input() nftCollectionLowBid = 0;
   @Input() isForSaleOnly: boolean = false;
   nftLastAcceptedBidAmountNanos: number;
+  nftMinBidAmountNanos: number;
 
   @Input() showNFTDetails = false;
   @Input() showExpandedNFTDetails = false;
@@ -197,6 +198,9 @@ export class FeedPostComponent implements OnInit {
         this.lowBid = _.minBy(this.availableSerialNumbers, "HighestBidAmountNanos")?.HighestBidAmountNanos || 0;
         if (this.nftEntryResponses.length === 1) {
           this.nftLastAcceptedBidAmountNanos = this.nftEntryResponses[0].LastAcceptedBidAmountNanos;
+          if (this.nftEntryResponses[0].MinBidAmountNanos>0) {
+            this.nftMinBidAmountNanos = this.nftEntryResponses[0].MinBidAmountNanos;
+          }
         }
       });
   }
