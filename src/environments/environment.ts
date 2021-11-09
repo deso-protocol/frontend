4de4db419005cdf672eda890e1431cdfe529dcc7
maebeam <maebeam0@gmail.com>
2021-11-09 17:11:30 -0500

    node.deso.org

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,9 +4,9 @@
 
 export const environment = {
   production: false,
-  uploadImageHostname: "bitclout.com",
-  jumioEndpointHostname: "bitclout.com",
-  uploadVideoHostname: "bitclout.com",
+  uploadImageHostname: "node.deso.org",
+  jumioEndpointHostname: "node.deso.org",
+  uploadVideoHostname: "node.deso.org",
   identityURL: "https://identity.deso.org",
   supportEmail: "",
   dd: {
