3254965f29dde65598b20201285ca8ea37b424e2
Marlon Montgomery <marlon.montgomery@globant.com>
2021-11-08 22:23:28 -0600

    Revert "update access level "
    This reverts commit 7f93a4322a6c3da182e47e35e60a82d2fe07ada8.

--- src/app/global-vars.service.ts
+++ src/app/global-vars.service.ts
@@ -849,7 +849,7 @@ export class GlobalVarsService {
 
   launchIdentityFlow(event: string): void {
     this.logEvent(`account : ${event} : launch`);
-    this.identityService.launch("/log-in?accessLevelRequesT=4", { referralCode: this.referralCode(), hideJumio: true }).subscribe((res) => {
+    this.identityService.launch("/log-in", { referralCode: this.referralCode(), hideJumio: true }).subscribe((res) => {
       this.logEvent(`account : ${event} : success`);
       this.backendApi.setIdentityServiceUsers(res.users, res.publicKeyAdded);
       this.updateEverything().add(() => {
