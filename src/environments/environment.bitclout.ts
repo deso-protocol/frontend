16cccdaca33c3eb17948cc7c16b275a1e8957f0d
lazynina <lazynina84@gmail.com>
2021-11-29 12:23:42 -0500

    add https:// prefix to verification endpoint hostname - this fixes issues running the frontend locally

--- src/environments/environment.bitclout.ts
+++ src/environments/environment.bitclout.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: true,
   uploadImageHostname: "node.deso.org",
-  verificationEndpointHostname: "node.deso.org",
+  verificationEndpointHostname: "https://node.deso.org",
   uploadVideoHostname: "node.deso.org",
   identityURL: "https://identity.bitclout.com",
   supportEmail: "node.admin@bitclout.com",
