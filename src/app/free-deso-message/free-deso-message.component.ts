361f9fd93cffae7aaddcaef5bd67089a5ceaf28d
lazynina <lazynina84@gmail.com>
2021-12-21 11:54:42 -0500

    add disclaimer on referrals page about amounts varying by locality of ID

--- src/app/free-deso-message/free-deso-message.component.ts
+++ src/app/free-deso-message/free-deso-message.component.ts
@@ -1,10 +1,12 @@
-import { Component } from "@angular/core";
+import { Component, Input } from "@angular/core";
 import { GlobalVarsService } from "../global-vars.service";
 
 @Component({
   selector: "free-deso-message",
-templateUrl: "./free-deso-message.component.html",
+  templateUrl: "./free-deso-message.component.html",
 })
 export class FreeDesoMessageComponent {
+  @Input() hideMessage: boolean = false;
+
   constructor(public globalVars: GlobalVarsService) {}
 }
