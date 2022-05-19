33402135bd86b2b6c9b468da25139845537b5778
jlmart88 <3260193+jlmart88@users.noreply.github.com>
2022-05-19 14:54:42 -0700

    Upgrade ingress for kube 1.22 (#13)
    Co-authored-by: Justin Martinez <justin@valorcode.tech>

--- terraform-environments/aws/dev/helm/70-gem-frontend/main.tf
+++ terraform-environments/aws/dev/helm/70-gem-frontend/main.tf
@@ -88,7 +88,7 @@ module "app" {
   # This is what you want to name the chart when deploying
   user_chart_name = local.fullnameOverride
   # The helm chart version you want to use
-  helm_version = "1.0.11"
+  helm_version = "1.0.19"
   # The namespace you want to install the chart into - it will create the namespace if it doesnt exist
   namespace = local.namespace
   # The helm chart values file
