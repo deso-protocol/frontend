51fe45a8e75c5b3f8253e32600893f328de87383
piratekev <piratekev@users.noreply.github.com>
2022-07-28 03:45:19 +0000

    Automated Change | Deploy to stage | tags: v2.1.1.0

--- terraform-environments/aws/stage/helm/70-gem-frontend/main.tf
+++ terraform-environments/aws/stage/helm/70-gem-frontend/main.tf
@@ -5,7 +5,7 @@ locals {
   fullnameOverride  = "gem-frontend"
   replica_count     = 1
   docker_repository = "283278994941.dkr.ecr.us-east-1.amazonaws.com/frontend"
-  docker_tag        = "v2.2.1.0"
+  docker_tag        = "v2.1.1.0"
   requests_memory   = "1Gi"
 
   tags = {
