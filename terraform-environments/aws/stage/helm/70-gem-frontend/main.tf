55f1e8482e99e9685cd35418e42863b2fe12dc75
Kevin Ayling <kevin@yourgem.app>
2022-07-27 17:52:47 -0700

    update tf config to v1beta1

--- terraform-environments/aws/stage/helm/70-gem-frontend/main.tf
+++ terraform-environments/aws/stage/helm/70-gem-frontend/main.tf
@@ -56,7 +56,7 @@ provider "helm" {
     host                   = data.terraform_remote_state.eks.outputs.cluster_endpoint
     cluster_ca_certificate = base64decode(data.terraform_remote_state.eks.outputs.cluster_certificate_authority_data)
     exec {
-      api_version = "client.authentication.k8s.io/v1alpha1"
+      api_version = "client.authentication.k8s.io/v1beta1"
       args        = ["eks", "get-token", "--cluster-name", local.environment_name]
       command     = "aws"
     }
