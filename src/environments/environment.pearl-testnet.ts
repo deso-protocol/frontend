2d9ab7049dffd1ffa5414c3eec3a6f2d55f73606
Kevin Ayling <kevin@yourgem.app>
2022-11-16 13:16:33 -0800

    build for testnet env

---
+++ src/environments/environment.pearl-testnet.ts
@@ -0,0 +1,25 @@
+export const environment = {
+  production: true,
+  uploadImageHostname: "node.deso.org",
+  verificationEndpointHostname: "https://node.deso.org",
+  uploadVideoHostname: "node.deso.org",
+  identityURL: "https://identity.deso.org",
+  supportEmail: "",
+  localNodeURL: "https://api.testnet.pearl.app",
+  dd: {
+    apiKey: undefined,
+    jsPath: undefined,
+    ajaxListenerPath: undefined,
+    endpoint: undefined,
+  },
+  amplitude: {
+    key: undefined,
+    domain: undefined,
+  },
+  node: {
+    id: 1,
+    name: "DeSo",
+    url: "https://node.deso.org",
+    logoAssetDir: "/assets/deso/",
+  },
+};
